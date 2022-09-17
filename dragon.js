88dc3e78aa9d4198a90cd6252f70ff5b99ffa660
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2022-09-17 10:13:17 +0700

    add traits

--- dragon.js
+++ dragon.js
@@ -1,14 +1,28 @@
+const TRAITS = require("./traits.json");
+
 const DEFAULT_PROPERTIES = {
   name: "Dragon",
   get birthdate() {
     return new Date().toISOString();
   },
+  get randomTraits() {
+    const traits = [];
+    TRAITS.forEach((TRAIT) => {
+      const traitType = TRAIT.type;
+      const traitValues = TRAIT.values;
+      const traitValue =
+        traitValues[Math.floor(Math.random() * traitValues.length)];
+      traits.push({ traitType, traitValue });
+    });
+    return traits;
+  },
 };
 
 class Dragon {
-  constructor({ birthdate, name } = {}) {
+  constructor({ birthdate, name, traits } = {}) {
     this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
     this.name = name || DEFAULT_PROPERTIES.name;
+    this.traits = traits || [];
   }
 }
 
